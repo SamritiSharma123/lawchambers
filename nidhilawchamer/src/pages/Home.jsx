afda9a67f905c9b8da1b44e19b75445681f83ef1
SamritiSharma123 <156428523+SamritiSharma123@users.noreply.github.com>
2026-03-22 23:28:30 +0530

    Update Home.jsx

--- nidhilawchamer/src/pages/Home.jsx
+++ nidhilawchamer/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import About from "../components/About";
 import Practice from "../components/Practice";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-import { motion } from "framer-motion";import { section } from "framer-motion/client";
+import { motion } from "framer-motion";
 
 export default function Home(){
   return(
